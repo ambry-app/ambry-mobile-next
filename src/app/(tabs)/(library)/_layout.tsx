c142e740eba0e34e1735c33d008442e3ab3130a3
Chris Dosé <chris@xn--dos-dma.com>
2024-10-05 09:42:05 -0700

    Simplify app init process

--- src/app/(tabs)/(library)/_layout.tsx
+++ src/app/(tabs)/(library)/_layout.tsx
@@ -1,27 +1,19 @@
-import LargeActivityIndicator from "@/src/components/LargeActivityIndicator";
-import { useAppInit } from "@/src/hooks/use.app.init";
 import { useSessionStore } from "@/src/stores/session";
 import { Redirect, Stack } from "expo-router";
-import { View } from "react-native";
 
 export default function AppLayout() {
-  const { isReady } = useAppInit();
   const session = useSessionStore((state) => state.session);
 
   if (!session) {
     return <Redirect href="/sign-in" />;
   }
 
-  return isReady ? (
+  return (
     <Stack>
       <Stack.Screen name="index" options={{ title: "Library" }} />
       <Stack.Screen name="media/[id]" options={{ title: "" }} />
       <Stack.Screen name="person/[id]" options={{ title: "" }} />
       <Stack.Screen name="series/[id]" options={{ title: "" }} />
     </Stack>
-  ) : (
-    <View className="flex h-full items-center justify-center">
-      <LargeActivityIndicator />
-    </View>
   );
 }
--- src/hooks/use.app.boot.ts
+++ src/hooks/use.app.boot.ts
@@ -1,20 +1,42 @@
 import migrations from "@/drizzle/migrations";
 import { db } from "@/src/db/db";
+import { syncDown } from "@/src/db/sync";
+import { useSessionStore } from "@/src/stores/session";
+import { useTrackPlayerStore } from "@/src/stores/trackPlayer";
 import { useMigrations } from "drizzle-orm/expo-sqlite/migrator";
 import { useEffect, useState } from "react";
 
 const useAppBoot = () => {
   const [isReady, setIsReady] = useState(false);
-  const { success, error } = useMigrations(db, migrations);
+  const { success: migrateSuccess, error: migrateError } = useMigrations(
+    db,
+    migrations,
+  );
+  const session = useSessionStore((_) => _.session);
+  const setupTrackPlayer = useTrackPlayerStore((_) => _.setupTrackPlayer);
+  const loadMostRecentMedia = useTrackPlayerStore((_) => _.loadMostRecentMedia);
 
   useEffect(() => {
-    if (!success) return;
-    if (error) {
-      console.error("Migration error", error);
-      return;
-    }
-    setIsReady(true);
-  }, [success, error]);
+    if (!session?.token) return setIsReady(true);
+    if (!migrateSuccess) return;
+    if (migrateError)
+      return console.error("[AppInit] migration error", migrateError);
+
+    console.log("[AppInit] starting...");
+    syncDown(session)
+      .then(() => console.log("[AppInit] db sync complete"))
+      .then(() => setupTrackPlayer())
+      .then(() => loadMostRecentMedia(session))
+      .then(() => console.log("[AppInit] trackPlayer setup complete"))
+      .catch((e) => console.error("[AppInit] error", e))
+      .finally(() => setIsReady(true));
+  }, [
+    loadMostRecentMedia,
+    setupTrackPlayer,
+    migrateSuccess,
+    migrateError,
+    session,
+  ]);
 
   return { isReady };
 };
