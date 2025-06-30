fb1023b516de612d875c3012a82598a00c8eedcd
Chris Dosé <chris@xn--dos-dma.com>
2025-06-29 19:43:13 -0700

    Simplify "authors and narrators" queries

--- src/db/library/get-media-authors-and-narrators.ts
+++ src/db/library/get-media-authors-and-narrators.ts
@@ -2,7 +2,7 @@ import { db } from "@/src/db/db";
 import * as schema from "@/src/db/schema";
 import { Session } from "@/src/stores/session";
 import { requireValue } from "@/src/utils/require-value";
-import { and, desc, eq, inArray } from "drizzle-orm";
+import { and, asc, eq } from "drizzle-orm";
 
 export type MediaAuthorOrNarrator = {
   id: string;
@@ -31,53 +31,13 @@ export async function getMediaAuthorsAndNarrators(
   mediaId: string,
 ) {
   const media = await getMedia(session, mediaId);
-  const bookAuthors = await getBookAuthors(session, media.bookId);
-  const authorIds = bookAuthors.map((ba) => ba.authorId);
-  const authors = await getAuthors(session, authorIds);
 
-  const mediaNarrators = await getMediaNarrators(session, mediaId);
-  const narratorIds = mediaNarrators.map((mn) => mn.narratorId);
-  const narrators = await getNarrators(session, narratorIds);
-
-  const authorPersonIds = new Set(authors.map((a) => a.personId));
-  const narratorPersonIds = new Set(narrators.map((n) => n.personId));
-  const personIds = [...new Set([...authorPersonIds, ...narratorPersonIds])];
-  const people = await getPeople(session, personIds);
-
-  const authorsMap = Object.fromEntries(authors.map((a) => [a.id, a]));
-  const narratorsMap = Object.fromEntries(narrators.map((n) => [n.id, n]));
-  const peopleMap = Object.fromEntries(people.map((p) => [p.id, p]));
-
-  const orderedAuthors = bookAuthors.map((ba) => {
-    const author = requireValue(authorsMap[ba.authorId], "Author not found");
-    const person = requireValue(peopleMap[author.personId], "Person not found");
-
-    return {
-      type: "author" as const,
-      name: author.name,
-      person: person,
-    };
-  });
-
-  const orderedNarrators = mediaNarrators.map((mn) => {
-    const narrator = requireValue(
-      narratorsMap[mn.narratorId],
-      "Narrator not found",
-    );
-    const person = requireValue(
-      peopleMap[narrator.personId],
-      "Person not found",
-    );
-    return {
-      type: "narrator" as const,
-      name: narrator.name,
-      person: person,
-    };
-  });
+  const authors = await getAuthors(session, media.bookId);
+  const narrators = await getNarrators(session, mediaId);
 
   const collapsedMap = new Map<string, MediaAuthorOrNarrator>();
 
-  for (const entry of [...orderedAuthors, ...orderedNarrators]) {
+  for (const entry of [...authors, ...narrators]) {
     const personId = entry.person.id;
     let rec = collapsedMap.get(personId);
 
@@ -100,8 +60,6 @@ export async function getMediaAuthorsAndNarrators(
   return [...collapsedMap.values()];
 }
 
-// --- Private helpers ---
-
 async function getMedia(session: Session, mediaId: string) {
   const rows = await db
     .select({
@@ -115,88 +73,83 @@ async function getMedia(session: Session, mediaId: string) {
   return requireValue(rows[0], "Media not found");
 }
 
-async function getBookAuthors(session: Session, bookId: string) {
-  return db
-    .select({
-      id: schema.bookAuthors.id,
-      authorId: schema.bookAuthors.authorId,
-    })
-    .from(schema.bookAuthors)
-    .where(
-      and(
-        eq(schema.bookAuthors.url, session.url),
-        eq(schema.bookAuthors.bookId, bookId),
-      ),
-    )
-    .orderBy(desc(schema.bookAuthors.insertedAt));
-}
-
-async function getAuthors(session: Session, authorIds: string[]) {
-  if (!authorIds.length) return [];
-
-  return db
+async function getAuthors(session: Session, bookId: string) {
+  const rows = await db
     .select({
       id: schema.authors.id,
       name: schema.authors.name,
       personId: schema.authors.personId,
+      person: {
+        id: schema.people.id,
+        name: schema.people.name,
+        thumbnails: schema.people.thumbnails,
+      },
     })
     .from(schema.authors)
+    .innerJoin(
+      schema.bookAuthors,
+      and(
+        eq(schema.bookAuthors.url, schema.authors.url),
+        eq(schema.bookAuthors.authorId, schema.authors.id),
+      ),
+    )
+    .innerJoin(
+      schema.people,
+      and(
+        eq(schema.people.url, schema.authors.url),
+        eq(schema.people.id, schema.authors.personId),
+      ),
+    )
     .where(
       and(
         eq(schema.authors.url, session.url),
-        inArray(schema.authors.id, authorIds),
-      ),
-    );
-}
-
-async function getMediaNarrators(session: Session, mediaId: string) {
-  return db
-    .select({
-      id: schema.mediaNarrators.id,
-      narratorId: schema.mediaNarrators.narratorId,
-    })
-    .from(schema.mediaNarrators)
-    .where(
-      and(
-        eq(schema.mediaNarrators.url, session.url),
-        eq(schema.mediaNarrators.mediaId, mediaId),
+        eq(schema.bookAuthors.bookId, bookId),
       ),
     )
-    .orderBy(desc(schema.mediaNarrators.insertedAt));
+    .orderBy(asc(schema.bookAuthors.insertedAt));
+
+  return rows.map((row) => ({
+    ...row,
+    type: "author" as const,
+  }));
 }
 
-async function getNarrators(session: Session, narratorIds: string[]) {
-  if (!narratorIds.length) return [];
-
-  return db
+async function getNarrators(session: Session, mediaId: string) {
+  const rows = await db
     .select({
       id: schema.narrators.id,
       name: schema.narrators.name,
-      personId: schema.narrators.personId,
+      person: {
+        id: schema.people.id,
+        name: schema.people.name,
+        thumbnails: schema.people.thumbnails,
+      },
     })
     .from(schema.narrators)
+    .innerJoin(
+      schema.mediaNarrators,
+      and(
+        eq(schema.mediaNarrators.url, schema.narrators.url),
+        eq(schema.mediaNarrators.narratorId, schema.narrators.id),
+      ),
+    )
+    .innerJoin(
+      schema.people,
+      and(
+        eq(schema.people.url, schema.narrators.url),
+        eq(schema.people.id, schema.narrators.personId),
+      ),
+    )
     .where(
       and(
         eq(schema.narrators.url, session.url),
-        inArray(schema.narrators.id, narratorIds),
+        eq(schema.mediaNarrators.mediaId, mediaId),
       ),
-    );
-}
+    )
+    .orderBy(asc(schema.mediaNarrators.insertedAt));
 
-async function getPeople(session: Session, personIds: string[]) {
-  if (!personIds.length) return [];
-
-  return db
-    .select({
-      id: schema.people.id,
-      name: schema.people.name,
-      thumbnails: schema.people.thumbnails,
-    })
-    .from(schema.people)
-    .where(
-      and(
-        eq(schema.people.url, session.url),
-        inArray(schema.people.id, personIds),
-      ),
-    );
+  return rows.map((row) => ({
+    ...row,
+    type: "narrator" as const,
+  }));
 }
